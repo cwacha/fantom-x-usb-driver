185e74b8892957212644cce0788b4abe41450e3d
Clemens Wacha <clemens@wacha.ch>
2025-05-27 23:56:55 +0200

    added first round of logic for pkgmake script and modified INF file to support new OS

--- pkg/pkgmake.ps1
+++ pkg/pkgmake.ps1
@@ -4,18 +4,21 @@ param (
 
 $BASEDIR = $PSScriptRoot
 #echo BASEDIR=$BASEDIR
+$CA_NAME = "Fantom-X Custom Driver CA"
 
 function all {
     "# building: $app_pkgname"
     clean
     import
+    setupcert
+    sign
     pkg
 }
 
 function _init {
     $global:app_pkgid = "fantom-x-usb-driver-win11"
     $global:app_displayname = "Fantom X USB Driver for Windows 10/11"
-    $global:app_version = Get-ChildItem $BASEDIR\..\ext\*.zip | % { $_.Name -replace "\S+-", "" -replace "[a-z]*.zip", "" }
+    $global:app_version = "1.0.0"
     $global:app_revision = git rev-list --count HEAD
     $global:app_build = git rev-parse --short HEAD
 
@@ -35,12 +38,86 @@ function _template {
     }
 }
 
+function _isAdmin {
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 function import {
     "# import ..."
-    mkdir BUILD/root -ea SilentlyContinue *> $null
+    mkdir BUILD -ea SilentlyContinue *> $null
 
-    Expand-Archive -Path $BASEDIR\..\ext\*.zip -DestinationPath BUILD/root
+    Expand-Archive -Path $BASEDIR\..\ext\*.zip -DestinationPath BUILD
+    mv BUILD\fanx_* BUILD\root
+    cd BUILD\root
+    rm Setup.exe
+    rm Uninstall.exe
+    rm Readme.htm
+    rm -r -fo Readme
+    cp -r -fo Files\64bit\Files\* .
+    rm -r -fo Files
+    rm RdDrvInf.dat
+    rm RdUninst.dat
+    rm Setup.dat
+    cd ../..
+
     cp -r -fo ..\src\* BUILD/root
+}
+
+function setupcert {
+    "# setupcert ..."
+
+    # Check if CA exists and create if is necessary
+    $cacert = Get-ChildItem Cert:\LocalMachine\Root | Where-Object Subject -like "CN=$CA_NAME"
+    if (!($cacert)) {
+        if (!(_isAdmin)) {
+            "### ERROR: The CA certificate '$CA_NAME' is missing, but you are not running as Admin"
+            exit 1
+        }
+
+        "# creating new CA for '$CA_NAME'"
+        $params = @{
+            Type              = 'Custom'
+            Subject           = "CN=$CA_NAME"
+            KeyUsage          = 'DigitalSignature'
+            CertStoreLocation = 'Cert:\LocalMachine\Root'
+        }
+        $cacert = New-SelfSignedCertificate @params
+        Export-Certificate -Cert $cacert -FilePath tmp.cer
+        Import-Certificate -StoreLocation Cert:\LocalMachine\TrustedPublisher -CertFilePath tmp.cer
+        Remove-Item tmp.cer
+    }
+    Export-Certificate -Cert $cacert -FilePath BUILD\root\CA_certificate.cer *> $null
+}
+
+function sign {
+    "# sign ..."
+
+    # check inf2cat.exe is available
+    if (!(Get-Command "inf2cat.exe" -ea SilentlyContinue)) {
+        "### ERROR: inf2cat.exe is missing in the path. Download and install the Windows Driver Kit (WDK) and setup the path correctly."
+        exit 1
+    }
+
+    & Inf2Cat.exe /os:10_x64 /driver:$BASEDIR\BUILD\root
+
+    # check signtool is available
+    if (!(Get-Command "signtool.exe" -ea SilentlyContinue)) {
+        "### ERROR: signtool.exe is missing in the path. Download and install the Windows Driver Kit (WDK) and setup the path correctly."
+        exit 1
+    }
+
+    # sign drivers
+    $files = @(
+        "*.cat",
+        "*.dll",
+        "*.exe",
+        "*.sys"
+    )
+    foreach ($file in $files) {
+        & signtool.exe sign /fd SHA256 /sm /s Root /n $CA_NAME /t http://timestamp.digicert.com BUILD\root\$file
+    }
+    ""
 }
 
 function pkg {
@@ -51,23 +128,6 @@ function pkg {
     Compress-Archive -Path root\* -DestinationPath ..\PKG\$app_pkgname.zip
     cd ..
     "## created $BASEDIR\PKG\$app_pkgname.zip"
-}
-
-function nupkg {
-    if (!(Get-Command "choco.exe" -ea SilentlyContinue)) {
-        return
-    }
-    "# packaging nupkg ..."
-    mkdir PKG *> $null
-
-    #rm -r -fo -ea SilentlyContinue BUILD\root
-    cp -r -fo nupkg PKG
-    cp -r -fo BUILD\* PKG\nupkg\tools
-    _template nupkg\package.nuspec | Out-File -Encoding "UTF8" PKG\nupkg\$app_pkgid.nuspec
-    rm PKG\nupkg\package.nuspec
-    cd PKG\nupkg
-    choco pack -outputdirectory $BASEDIR\PKG
-    cd $BASEDIR
 }
 
 function clean {
